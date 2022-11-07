1ea2498cd5077a9bc03e8e04f2bfaa33d2e0cb19
dulal <dulalmiah70112@gmail.com>
2022-11-07 08:33:11 +0600

    Create three activity. Use of sharedPreference and Data Pass.

--- app/src/main/java/com/dulali/miniproject/MainActivity.java
+++ app/src/main/java/com/dulali/miniproject/MainActivity.java
@@ -1,7 +1,11 @@
 package com.dulali.miniproject;
 
+import static java.lang.Thread.sleep;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +14,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    sleep(4000);
+                    SharedPreferences perf = getSharedPreferences("login" , MODE_PRIVATE);
+                    Boolean check = perf.getBoolean("flag" , false);
+
+                    if(check){
+                        startActivity(new Intent(MainActivity.this , HomeActivity.class));
+                    }
+                    else{
+                        startActivity(new Intent(MainActivity.this , LoginActivity.class));
+                    }
+
+
+
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        thread.start();
     }
 }
